67bf33be168d2ff2fdff1dfdfde7d76c730006b1
Matheus97px <matheus97p.q@gmail.com>
2025-07-11 13:04:34 -0300

    refactor: conta

--- Menu.ts
+++ Menu.ts
@@ -1,6 +1,5 @@
 import readLine = require('readline-sync');
 import { colors } from './src/util/Colors';
-import { Conta } from './src/model/Conta';
 import { ContaCorrente } from './src/model/ContaCorrente';
 import { ContaPoupanca } from './src/model/ContaPoupanca';
 
